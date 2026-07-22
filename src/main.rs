706a0eccd21cbe36124fd43c245f7b2050c6610a
Gaëtan Trellu <gaetan.trellu@gmail.com>
2026-07-22 02:28:18 -0400

    fix: stop cleanly on termination signals
    (cherry picked from commit 83e5dfe5bd21e8d849f78f94fa9f052e9841162e)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
 use ovos_messagebus::{Config, MessageBus};
+use tracing::info;
 use tracing_subscriber::EnvFilter;
+
+#[cfg(unix)]
+async fn shutdown_signal() -> std::io::Result<()> {
+    use tokio::signal::unix::{signal, SignalKind};
+
+    let mut terminate = signal(SignalKind::terminate())?;
+    tokio::select! {
+        result = tokio::signal::ctrl_c() => result?,
+        _ = terminate.recv() => {},
+    }
+    Ok(())
+}
+
+#[cfg(not(unix))]
+async fn shutdown_signal() -> std::io::Result<()> {
+    tokio::signal::ctrl_c().await
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -9,6 +27,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let config = Config::new();
     let message_bus = MessageBus::new(config);
-    message_bus.run().await?;
+    tokio::select! {
+        result = message_bus.run() => result?,
+        result = shutdown_signal() => {
+            result?;
+            info!("Shutdown signal received");
+        },
+    }
     Ok(())
 }
